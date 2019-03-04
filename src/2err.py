98f24e29f05b1dd8071326a010b7c372be7c50fd
nicosembiring <nicosembiring52@gmail.com>
2019-03-04 21:52:56 +0700

    nico

---
+++ src/2err.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 11:39:52 2019
+
+@author: Nico Sembiring
+"""
+
+a = 2
+b = "4"
+
+try:
+    c = a + b
+    print(c)
+except TypeError:
+    print ("Beda Tipe Data Cuk")
