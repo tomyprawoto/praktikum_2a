2e0e131108ee302ec058ce931eabe6871ea97e37
MuhRifkyPrananda <ikkyrifky08@gmail.com>
2019-03-04 23:20:12 +0700

    iky

---
+++ src/1174017.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 11:51:40 2019
+
+@author: ASUS
+"""
+#nomor 1
+print ("***   ***   ************   ***  ***   **********   ***   ************")
+print ("***   ***   ************   ***  ***   **********   ***   ***********")
+print ("***   ***          ***     ***  ***   **      **   ***          ***")
+print ("***   ***         ***      ***  ***   **      **   ***         ***")
+print ("***   ***        ***       ********   **      **   ***        ***")
+print ("***   ***       ***             ***   **      **   ***       ***")
+print ("***   ***      ***              ***   **      **   ***      ***")
+print ("***   ***     ***               ***   **      **   ***     ***")
+print ("***   ***    ***                ***   **********   ***    ***")
+print ("***   ***   ***                 ***   **********   ***   ***")
+
+#nomor 2
+npm = input("Masukan NPM :")
+hitung = 0
+while(hitung <= 87):
+    print("Halo, " + str(npm) + " Apa kabar ?")
+    hitung = hitung +1 
+    
+#nomor 3
+npm = input("Masukan NPM :")
+hitung = 0
+while(hitung <= 15):
+    print("Halo, " + str(npm[4:7]) + " Apa kabar ?")
+    hitung = hitung +1
+    
+#nomor 4
+npm = input("Masukan NPM :")
+print("Halo, " + str(npm[-3]) + " Apa Kabar?")
+
+#nomor 5
+a = 1
+b = 1
+c = 7
+d = 4
+e = 0
+f = 1
+g = 7
+h = a+b+c+d+e+f+g
+i = a*b*c*d*e*f*g
+
+print(a,b,c,d,e,f,g)
+
+#nomor 6
+print(h)
+
+#nomor 7
+print(i)
+
+#nomor 8
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+#nomor 9
+print(d,g)
+
+#nomor 10
+print(a,b,c,f)
+
+#nomor 11
+print(a,b,c,)
