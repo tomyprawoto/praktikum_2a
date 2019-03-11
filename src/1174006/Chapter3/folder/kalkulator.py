98c8673f61abaf133177c32080204cb013c2d5f5
Diva Krishna <divakrishnam@gmail.com>
2019-03-11 19:54:59 +0700

    Chapter 3 Diva

---
+++ src/1174006/Chapter3/folder/kalkulator.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 09:18:02 2019
+
+@author: divakrishna
+"""
+
+def Penambahan(a, b):
+    r = a + b
+    return r
+
+def Pengurangan(a, b):
+    r = a - b
+    return r
+
+def Perkalian(a, b):
+    r = a * b
+    return r
+
+def Pembagian(a, b):
+    r = a / b
+    return r
+    
